dce30d3cbd38739a04872fe9e661b3e92e4ebc76
hannahheflin <hannahgh02@hotmail.com>
2022-11-12 18:27:21 -0600

    .

--- tests.py
+++ tests.py
@@ -1,1 +1,24 @@
+import functions
 
+# tests for openFile
+
+
+# tests for numbers
+
+
+# tests for dist
+
+
+# tests for isPalindrone
+
+
+# tests for divide
+
+
+# tests for sq
+
+
+# tests for greetUser
+
+
+# tests for displayItem
