dfcdad3e83fce57c009471516f04fb296273b9d8
leeyat1 <111946943+leeyat1@users.noreply.github.com>
2022-11-30 17:58:42 -0600

    Update orderHistory.py

--- orderHistory.py
+++ orderHistory.py
@@ -1,31 +1,14 @@
 import csv
-import pandas
+import pandas as pd
+
 
 class orderHistory:
-
     def __init__(self, username):
         self.username = username
-        self.productID = None
-        self.quantity = 0
-        self.total = 0
-        self.cardInfo = 0
-
-    def getQuant(self):
-        data = []
-        with open('Cart.csv') as csv_file:
-            csv_reader = csv.reader(csv_file)
-            for row in csv_reader:
-                data.append(row)
-
-            col = [x[0] for x in data]
-            if self.username in col:
-                for x in range(0, len(data)):
-                    if self.username == data[x][0]:
-                        # print the card number
-                        # print("Quanitiy of Item:{}".format (data[x][2]))
-                        return data[x][2]
-            else:
-                print("User not found")
+        self.product_ID = None
+        self.quantity = None
+        self.total_price = None
+        self.cardInfo = None
 
     def getPID(self):
         data = []
@@ -38,61 +21,23 @@ class orderHistory:
             if self.username in col:
                 for x in range(0, len(data)):
                     if self.username == data[x][0]:
-                        # print("Product ID:{}".format (data[x][1]))
-                        # returns the product id
                         return data[x][1]
             else:
                 print("User not found")
 
-
-    def getCardInfo(self):
-        data = []
-        with open('customers.csv') as csv_file:
-            csv_reader = csv.reader(csv_file)
-            for row in csv_reader:
-                data.append(row)
-
-            col = [x[0] for x in data]
-            if self.username in col:
-                for x in range(0, len(data)):
-                    if self.username == data[x][0]:
-                        # print the card number
-                        # print("Card Used:{}".format (data[x][5]))
-                        return data[x][5]
-            else:
-                print("Card Info Not Found")
-
     def getTotal(self):
-        price = 0
-        quantity = 0
-        username = self.username
-        data1 = []  # cartInfo holds the quantity
-        data2 = []  # furniture info holds the price
-        # these are connected by ProductID
-        with open('Cart.csv') as csv_file:
-            csv_reader = csv.reader(csv_file)
-            for row in csv_reader:
-                data1.append(row)
-
-        col = [x[0] for x in data1]
-        if username in col:
-            for x in range(0, len(data1)):
-                if username == data1[x][0]:
-                    furnitureID = data1[x][1]
-                    quantity = data1[x][2]
-
         with open('Furniture.csv') as csv_file:
             csv_reader = csv.reader(csv_file)
             for row in csv_reader:
-                data2.append(row)
-            col = [x[0] for x in data2]
-            if furnitureID in col:
-                for x in range(0, len(data1)):
-                    if furnitureID == data1[x][0]:
-                        price = data2[x][4]
-                        total = price * quantity
+                if row[0].strip() == self.product_ID:
+                    return float(row[4]) * float(self.quantity)
 
-                        return total
+    def getCardInfo(self):
+        with open('customers.csv') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for row in csv_reader:
+                if row[0].strip() == self.username:
+                    return row[5]
 
     def addHistory(self, quant):
 
@@ -101,11 +46,10 @@ class orderHistory:
         self.total_price = self.getTotal()
         self.card_used = self.getCardInfo()
 
-        with open('orderHistory.csv', mode='w') as csv_file:
+        with open('orderHistory.csv', mode='a') as csv_file:
             csv_reader = csv.reader(csv_file)
             fieldnames = ['Username', 'ProductID', 'Item Quant', 'Payment Info']
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-            writer.writeheader()
             writer.writerow({'Username': self.username, 'ProductID': self.product_ID, 'Item Quant': self.quantity,
                              'Payment Info': self.card_used})
 
@@ -114,11 +58,24 @@ class orderHistory:
         with open('orderHistory.csv') as csv_file:
             csv_reader = csv.reader(csv_file)
             for row in csv_reader:
-                data.append(row)
+                if row[0].strip() == self.username:
+                    with open('Furniture.csv') as furniture:
+                        furn_reader = csv.reader(furniture)
+                        for row2 in furn_reader:
+                            if row2[0].strip() == row[1].strip():
+                                print('\nProduct ID:{}'.format(row[1]), '\nProduct Name: {}'.format(row2[1]), '\nCategory: {}'.format(row2[2]),
+                                      '\nDesigner: {}'.format(row2[3]), '\nPrice: {}'.format(row2[4]), '\nQuantity:{}'.format(row[2]), '\nTotal Price:{}'.format(row[3]), '\nCard Used:{}'.format(row[4]))
 
-            col = [x[0] for x in data]
-            if self.username in col:
-                print('Username:{}'.format(row[0]), '\nProduct ID:{}'.format(row[1]),
-                      '\nQuantity:{}'.format(row[2]), '\nCard Used:{}'.format(row[3]))
-            else:
-                print("No Order History")
+    def clearHistory(self):
+        lineNum = 0
+        linesFound = []
+        with open("orderHistory.csv", "r") as history:
+            historyCSV = csv.DictReader(history)
+            for line in historyCSV:
+                if line["Username"].strip() == self.username:
+                    linesFound.append(lineNum)
+                lineNum += 1
+        if len(linesFound) != 0:
+            df = pd.read_csv("orderHistory.csv")
+            df = df.drop(linesFound)
+            df.to_csv("orderHistory.csv", index=False)
